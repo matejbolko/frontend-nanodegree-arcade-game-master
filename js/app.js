3a73e38ec698afed847cf9b11fb2504a63196b4d
matejbolko <36517473+matejbolko@users.noreply.github.com>
2018-06-06 17:55:42 +0200

    player init and handling
    player initialized and added player handling.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
+var canvas = document.getElementById('mycanvas');
+const MAX_X = 505;
+const MAX_Y = 606;
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -24,12 +24,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+  this.x = x;
+  this.y = y;
+  this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.handleInput  = function(keyPress) {
+  if (keyPress == 'left' && this.x > 0){
+    this.x -= 101;
+  }
+  if (keyPress == 'right' && this.x < 400){
+    this.x += 101;
+  }
+  if (keyPress == 'up' && this.y > 0){
+    this.y -= 85;
+  }
+  if (keyPress == 'down' && this.y < 400){
+    this.y += 85;
+  }
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let player = new Player(200, 400);
+let allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
